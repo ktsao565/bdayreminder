ad87f27916610a4c686b9187bd6a275509eeeed0
Kevin Tsao <kevintsao@Davids-MBP.home>
2016-05-27 13:21:29 -0700

    Finished find state app

--- mysite/findstate/views.py
+++ mysite/findstate/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
-#longitude is x, latitude is y
-# Create your views here.
+
 import os
-import requests, json
-from django.http import HttpResponse, HttpResponseRedirect
+import json
+from django.http import HttpResponse
 from django.conf import settings
+from django.shortcuts import render
 
 
-def on_left(point,a,b):
+#Function to determine whether the line that extends horizontally to the
+#right of point will intersect with line segment (a,b)
+def on_left(point, a, b):
     if (point[1] < min(b[1],a[1])) or (point[1] > max(b[1],a[1])):
         return False
+
     slope = (b[1]-a[1])/(b[0]-a[0])
     y_intercept = b[1]-slope*b[0]
     x_line = (point[1] - y_intercept)/slope
@@ -21,24 +23,30 @@ def on_left(point,a,b):
 def index(request):
     return render(request,'findstate/index.html')
 
+
 def results(request):
     if 'latitude' in request.GET and 'longitude' in request.GET:
         latitude = float(request.GET['latitude'])
         longitude = float(request.GET['longitude'])
         states = []
-        results = []
+        result_states = []
         point = [longitude,latitude]
+
         with open(os.path.join(settings.BASE_DIR,'states.json')) as file_:
             for line in file_:
                 states.append(json.loads(line))
+
         for state in states:
-            result = state['border']
+            edges = state['border']
             in_state = False
-            for i in xrange(len(result)-1):
-                if on_left(point,result[i],result[i+1]):
+
+            for i in xrange(len(edges)-1):
+                if on_left(point, edges[i], edges[i+1]):
                     in_state = not in_state
+
             if in_state:
-                results.append(state['state'])
-        return HttpResponse("States: " + ''.join(results) + str(point))
+                result_states.append(state['state'])
+
+        return HttpResponse(str(point) + " is in state(s): " + ','.join(result_states))
     else:
         return HttpResponse("No input")
